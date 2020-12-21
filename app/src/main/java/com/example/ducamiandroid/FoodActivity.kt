ce4cf7ee117227949e869c0f2f9e16c54afadf65
kimjw2003 <kimjw111003@gmail.com>
2020-12-21 11:32:24 +0900

    주석

--- app/src/main/java/com/example/ducamiandroid/FoodActivity.kt
+++ app/src/main/java/com/example/ducamiandroid/FoodActivity.kt
@@ -29,18 +29,18 @@ class FoodActivity : AppCompatActivity() {
                 "D10", "7240393", time
         ).enqueue(object : retrofit2.Callback<FoodBase> {
             override fun onResponse(call: Call<FoodBase>, response: Response<FoodBase>) {
-                food1.text = "급식이없어요"
+                food1.text = "급식이없어요"       //foodText 다 초기화
                 food2.text = "급식이없어요"
                 food3.text = "급식이없어요"
 
-                response.body()?.mealServiceDietInfo?.let {
-                    val cnt = it[0].head?.get(0)?.list_total_count ?: return
+                response.body()?.mealServiceDietInfo?.let { //mealServiceInfo가 null이 아니라면 let실행
+                    val cnt = it[0].head?.get(0)?.list_total_count ?: return //cnt = list_total_count인데 list_total_count가 null이라면 return
 
-                    it[1].row?.let { list ->
-                        val regex = Regex("[0-9]+.")
-                        if (cnt >= 1) {
-                            food1.text = regex.replace(list[0].DDISH_NM ?: return, "")
-                                        .replace("<br/>", "\n")
+                    it[1].row?.let { list -> // row가 null이 아니라면
+                        val regex = Regex("[0-9]+.") //정규식 선언
+                        if (cnt >= 1) { //list_total_count가 1보다 같거나 작다면
+                            food1.text = regex.replace(list[0].DDISH_NM ?: return, "") //DDISH_NM에 regex가 선언해준 정규식에 걸리는게 있다면 그것을 없애준다
+                                    .replace("<br/>", "\n")                           // <br/>을 \n로 바꾼다
                         }
                         if (cnt >= 2) {
                             food2.text = regex.replace(list[1].DDISH_NM ?: return, "")
